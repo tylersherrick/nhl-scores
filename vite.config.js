a7a567308e3437a1f0a573449bac8a8ffb1ae34f
tylersherrick <tyler.sherrick@gmail.com>
2024-10-30 13:16:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/nhl-scores/', // replace 'your-repo-name' with your actual GitHub repo name
+});
